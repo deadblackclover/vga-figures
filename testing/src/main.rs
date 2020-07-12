86bd40aa7593145740d61293bf0264317ae42ca4
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-07-12 22:31:40 +0700

    Added test file

---
+++ testing/src/main.rs
@@ -0,0 +1,46 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(testing::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use testing::serial_print;
+
+use vga::colors::Color16;
+use vga::writers::{Graphics640x480x16, GraphicsWriter};
+use vga_figures::Figures;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let mode = Graphics640x480x16::new();
+    mode.set_mode();
+    mode.clear_screen(Color16::Black);
+
+    let figures = Figures::new(mode);
+    figures.rectangle(10, 10, 10, 10, Color16::White);
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_print!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    testing::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
